a341a1629c25c64eb5f016ce129416cb4d010061
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2025-03-02 19:06:25 +0300

    small button fix

--- lib/modules/main_page/presentation/main_page_screen.dart
+++ lib/modules/main_page/presentation/main_page_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:swap_chat_app/core/utils/widgets/background_gradient.dart';
 
@@ -7,48 +9,47 @@ class MainPageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        BackgroundGradient(),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              UserCard(),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: 128,
-                  height: 128,
-                  child: ClipOval(
-                    child: Material(
-                      child: InkWell(
-                        onLongPress: () {},
-                        child: Icon(
-                          Icons.mic,
-                          size: 32,
-                        ),
+      body: Stack(
+        children: [
+          BackgroundGradient(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                UserCard(),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 24),
+                  child: SizedBox(
+                    width: 128,
+                    height: 128,
+                    child: ClipOval(
+                      child: GestureDetector(
+                        onTapDown: (d) {
+                          log("start tap");
+                        },
+                        onTapUp: (e) {
+                          log("tap end");
+                        },
+                        child: Material(child: Icon(Icons.mic, size: 32)),
                       ),
                     ),
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        )
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
 
 class UserCard extends StatelessWidget {
-  const UserCard({
-    super.key,
-  });
+  const UserCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -64,9 +65,10 @@ class UserCard extends StatelessWidget {
                 child: Container(
                   decoration: BoxDecoration(color: Colors.white),
                   child: Image.network(
-                      width: 64,
-                      height: 64,
-                      "https://t4.ftcdn.net/jpg/02/34/12/57/360_F_234125704_SMg9xwLluzA4GLq46Vb5Cw9Pp0LNaUut.jpg"),
+                    width: 64,
+                    height: 64,
+                    "https://t4.ftcdn.net/jpg/02/34/12/57/360_F_234125704_SMg9xwLluzA4GLq46Vb5Cw9Pp0LNaUut.jpg",
+                  ),
                 ),
               ),
             ),
@@ -82,11 +84,11 @@ class UserCard extends StatelessWidget {
                 Text(
                   "Какая то дополнительная информация",
                   style: Theme.of(context).textTheme.bodySmall,
-                )
+                ),
               ],
             ),
             Spacer(),
-            IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
           ],
         ),
       ),
